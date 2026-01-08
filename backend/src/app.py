e7f5255518970ef7869ac13127d99af34abdde06
oMaDaSa <matheusdsantana07@gmail.com>
2026-01-08 20:44:48 -0300

    health check aws

--- backend/src/app.py
+++ backend/src/app.py
@@ -7,5 +7,9 @@ load_dotenv()
 app = Flask(__name__)
 app.register_blueprint(api_bp, url_prefix='/api')
 
+@app.route('/')
+def health_check():
+    return {"status": "alive"}, 200
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
