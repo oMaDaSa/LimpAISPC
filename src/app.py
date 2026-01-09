0e426bfac56aaf64ba34bcaa9d889543f7e131ac
oMaDaSa <matheusdsantana07@gmail.com>
2026-01-09 02:24:34 -0300

    port 8080

--- src/app.py
+++ src/app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 from api.routes import api_bp
 
@@ -16,5 +15,4 @@ def health_check():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=False)
